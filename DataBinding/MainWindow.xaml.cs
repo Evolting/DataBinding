2a34a0b765c4e0eee02aa6deb4b9843edb17e7b7
VieLongT <vielongt52@gmail.com>
2022-09-14 23:32:14 +0700

    Add project files.

---
+++ DataBinding/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Student st = new Student()
+        {
+            Id = 1,
+            Name = "Long"
+        };
+
+        List<Student> students = new List<Student>()
+        {
+            new Student() { Id = 2, Name = "Long", Major = new Major() { Id = 1, Name = "SE" } },
+            new Student() { Id = 3, Name = "Diep", Major = new Major() { Id = 2, Name = "GD" } }
+        };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = st;
+            lvStudent.ItemsSource = students;
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(st.Id + " " + st.Name);
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Edit done!");
+        }
+    }
+}
---
+++ DataBinding/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBinding
+{
+    internal class Student
+    {
+        private int id;
+        private string name;
+        private Major major;
+
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public Major Major { get => major; set => major = value; }
+    }
+}
